657a3075096e025a9eb851b3b51243ca14696a64
Alex <grigorealexandru.dev@gmail.com>
2019-03-10 20:52:13 +0200

    refactor

--- youtube.py
+++ youtube.py
@@ -41,36 +41,24 @@ def init():
 #     Custom Youtube api requests
 # =====================================
 
-def get_playlist_items_list(id, token='', maxResults=50):
+def _playlistitems_list(id, token='', maxResults=50):
     return client.playlistItems().list(
-        part='contentDetails',
+        part='snippet',
         maxResults=maxResults,
         playlistId=id,
         pageToken=token
     ).execute()
 
 
-def get_my_channel_playlists():
+def _channel_related_playlists(id=None, mine=None):
     return client.channels().list(
         part='contentDetails',
-        mine=True
+        id=id,
+        mine=mine
     ).execute()['items'][0]['contentDetails']['relatedPlaylists']
 
 
-def get_liked_videos():
-    videos = []
-    liked_id = get_my_channel_playlists()['likes']
-    response = get_playlist_items_list(liked_id)
-    token = response.get('nextPageToken')
-    while token:
-        for item in response['items']:
-            videos.append(item['contentDetails']['videoId'])
-        response = get_playlist_items_list(liked_id, token)
-        token = response.get('nextPageToken')
-    return videos
-
-
-def get_my_playlists(token=''):
+def _my_playlists_list(token=''):
     return client.playlists().list(
         part='snippet',
         mine=True,
@@ -79,16 +67,49 @@ def get_my_playlists(token=''):
     ).execute()
 
 
-def get_all_my_playlists():
-    playlists = []
-    response = get_my_playlists()
+def get_my_playlists():
+    def get_items():
+        return [{
+            'title': item['snippet']['title'],
+            'id': item['id']
+        } for item in response['items']]
+
+    response = _my_playlists_list()
     token = response.get('nextPageToken')
+    playlists = get_items()
     while token:
-        playlists.append(response['items'])
-        response = get_my_playlists(token)
+        response = _my_playlists_list(token)
         token = response.get('nextPageToken')
+        playlists.extend(get_items())
     return playlists
 
 
+def get_playlist_items(id, all=False):
+    def get_items():
+        return [{
+            'title': item['snippet']['title'],
+            'id': item['snippet']['resourceId']['videoId']
+        } for item in response['items']]
+
+    response = _playlistitems_list(id)
+    token = response.get('nextPageToken')
+    videos = get_items()
+    while token and all:
+        response = _playlistitems_list(id, token)
+        token = response.get('nextPageToken')
+        videos.extend(get_items())
+    return videos
+
+
+def get_liked_playlist():
+    liked_id = _channel_related_playlists(mine=True)['likes']
+    return get_playlist_items(liked_id, True)
+
+
+def get_uploads_playlist(id):
+    upload_id = _channel_related_playlists(id)['uploads']
+    return get_playlist_items(upload_id, True)
+
+
 def get_last_vid(id):
-    return get_playlist_items_list(id, maxResults=1)['items'][0]['contentDetails']['videoId']
+    return _playlistitems_list(id, maxResults=1)['items'][0]['contentDetails']['videoId']
